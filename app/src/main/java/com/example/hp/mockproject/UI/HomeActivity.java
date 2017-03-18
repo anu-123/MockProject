acbd2097c2eaed4d2124e88bfd9b4b39a381e36a
anu-123 <r.annu492@gmail.com>
2017-03-18 17:51:23 +0530

    navigation view without mvp

--- app/src/main/java/com/example/hp/mockproject/UI/HomeActivity.java
+++ app/src/main/java/com/example/hp/mockproject/UI/HomeActivity.java
@@ -1,15 +1,88 @@
 package com.example.hp.mockproject.UI;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.hp.mockproject.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private Toolbar home_toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initialise();
+        setNavigationDrawer();
+
+        setNavigationMenuItems();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.RIGHT);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setNavigationMenuItems() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_home:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setNavigationDrawer() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, home_toolbar, R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void initialise() {
+        home_toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        setSupportActionBar(home_toolbar);
+        getSupportActionBar().setTitle("HomePage");
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_name);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 }
